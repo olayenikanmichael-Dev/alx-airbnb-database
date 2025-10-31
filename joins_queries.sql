061b3609700d67ff6097b6419f141d509000c5c8
Olayenikan  mike <olayenikanmichaelolaniyi@gmail.com>
2025-10-31 11:53:33 +0100

    Create joins_queries.sql

---
+++ joins_queries.sql
@@ -0,0 +1,41 @@
+--INNER JOIN---
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+INNER JOIN users 
+    ON bookings.user_id = users.id;
+
+--LEFT JOIN--
+
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews 
+    ON properties.id = reviews.property_id;
+
+
+--FULL OUTER JOIN---
+
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings 
+    ON users.id = bookings.user_id;
+
+
