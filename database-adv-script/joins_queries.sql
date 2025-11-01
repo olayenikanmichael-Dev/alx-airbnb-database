b5dcadb1167daf796ab8e05a4a2e2144a96d0bed
Olayenikan  mike <olayenikanmichaelolaniyi@gmail.com>
2025-11-01 17:23:23 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -21,7 +21,9 @@ SELECT
     reviews.comment
 FROM properties
 LEFT JOIN reviews 
-    ON properties.id = reviews.property_id;
+    ON properties.id = reviews.property_id
+ORDER BY properties.id;
+
 
 
 --FULL OUTER JOIN---
