a5588ad904f2ff09a5fa43e6b205b89b2eb4ce37
Olayenikan  mike <olayenikanmichaelolaniyi@gmail.com>
2025-11-01 17:53:48 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -18,12 +18,13 @@ SELECT
     properties.id AS property_id,
     properties.name AS property_name,
     COUNT(bookings.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(bookings.id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(bookings.id) DESC) AS booking_rank
 FROM properties
 LEFT JOIN bookings 
     ON properties.id = bookings.property_id
 GROUP BY properties.id, properties.name
 ORDER BY booking_rank;
+
 
 
 
