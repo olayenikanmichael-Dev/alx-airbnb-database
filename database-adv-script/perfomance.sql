da4b6c168951c64e94e25cc8e55b7a1e05ed3b1d
Olayenikan  mike <olayenikanmichaelolaniyi@gmail.com>
2025-11-01 18:02:04 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,22 @@
+
+-- STEP 1: INITIAL COMPLEX QUERY (Unoptimized)
+
+
+-- Retrieve all bookings along with user details, property details, and payment details
+EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    u.id AS user_id,
+    u.name AS user_name,
+    p.id AS property_id,
+    p.name AS property_name,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status,
+    b.start_date,
+    b.end_date
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.id = pay.booking_id
+ORDER BY b.start_date DESC;
